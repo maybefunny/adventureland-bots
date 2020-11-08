aa80fd7ce48b93ffd19b5a414a593721962e4094
Kent Rasmussen <hyprkookeez@gmail.com>
2020-11-08 11:41:16 +0900

    Energize other mages

--- source/phoenix.ts
+++ source/phoenix.ts
@@ -668,6 +668,19 @@ async function startMage(bot: Mage, n: number) {
             }
 
             if (target && bot.canUse("attack")) {
+                // Energize if we can
+                if (!bot.character.s.energized) {
+                    for (const mage of [earthMag, earthMag2, earthMag3]) {
+                        if (!mage) continue // Not online
+                        if (!mage.canUse("energize")) continue // Can't energize
+                        if (mage.character.id == bot.character.id) continue // Can't energize ourself (TODO: is this true?)
+                        if (Tools.distance(bot.character, earthMag.character) > bot.G.skills.energize.range) continue // Too far away
+
+                        mage.energize(bot.character.id)
+                        break
+                    }
+                }
+
                 await bot.attack(target.id)
             }
         } catch (e) {
